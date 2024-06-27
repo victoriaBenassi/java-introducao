2528fef2443fb1f56b084b00bf4f7edc61e3b673
victoriaBenassi <benassi.victoria@hotmail.com>
2024-06-26 21:17:35 -0300

    adicionando pacotes sobre metodos

---
+++ metodos/src/Metodos.java
@@ -0,0 +1,8 @@
+public class Metodos {
+    //criterio de nomeação de métodos:
+    // deve ser nomeado como verbo;
+    // seguir o padrão camelCase;
+
+    //<visibilidade> <retorno> <nomeMetodo> (<parametro>){<bloco de comando>}
+    public void somar(int n1, int n2){}
+}
