3e0a2b2badf87e2848aaa95f0c46ed4a50c778de
victoriaBenassi <benassi.victoria@hotmail.com>
2024-06-30 20:49:47 -0300

    adicionando pacotes sobre metodos

--- metodos/src/Metodos.java
+++ metodos/src/Metodos.java
@@ -5,4 +5,12 @@ public class Metodos {
 
     //<visibilidade> <retorno> <nomeMetodo> (<parametro>){<bloco de comando>}
     public void somar(int n1, int n2){}
+
+    // throws Exception: indica que o método ao ser utilizado pode gerar uma exceção
+    public double dividir(int dividendo, int divisor) throws Exception {
+        if (divisor == 0) {
+            throw new Exception("Divisão por zero não é permitida.");
+        }
+        return (double) dividendo / divisor;
+    }
 }
