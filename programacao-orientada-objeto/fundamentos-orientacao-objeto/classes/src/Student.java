f7b39aa2d6159b1feab726f9348c755861c99e55
victoriaBenassi <benassi.victoria@hotmail.com>
2024-07-07 18:06:40 -0300

    adicionando arquivos sobre classes

---
+++ programacao-orientada-objeto/fundamentos-orientacao-objeto/classes/src/Student.java
@@ -0,0 +1,11 @@
+//criando a classe student
+// com todas as caracteristicas e comportamentos aplicados
+public class Student {
+    
+        String name;
+        int age;
+
+        void running(){
+            //NOSSO CÓDIGO AQUI
+          }
+    }
