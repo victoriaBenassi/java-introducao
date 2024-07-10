e77e75cf72de5d8651d3eb59c0558ebb6da14d8a
victoriaBenassi <benassi.victoria@hotmail.com>
2024-07-09 22:34:35 -0300

    adicionando pacotes sobre interface na POO

---
+++ programacao-orientada-objeto/interface/src/equipamentos/multifuncional/Multifuncional.java
@@ -0,0 +1,19 @@
+package equipamentos.multifuncional;
+
+import equipamentos.copiadora.Copiadora;
+import equipamentos.digitalizadora.Digitalizadora;
+import equipamentos.impressora.Impressora;
+
+public class Multifuncional implements Copiadora, Digitalizadora, Impressora {
+
+    public void imprimir () {
+        System.out.println("IMPRIMINDO VIA EQUIPAMENTO MULTIFUNCIONAL");     
+    }
+    public void digitalizar() {
+        System.out.println("DIGITALIZANDO VIA EQUIPAMENTO MULTIFUNCIONAL");
+    }
+    public void copiar() {
+        System.out.println("COPIANDO VIA EQUIPAMENTO MULTIFUNCIONAL");    
+    }
+    
+}
